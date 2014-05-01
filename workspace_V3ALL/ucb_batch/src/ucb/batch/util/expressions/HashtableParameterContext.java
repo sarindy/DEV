6a4e36c235af27f3014ea2444158885dddca5dec
sarindy <sarindyo@yahoo.com>
2014-05-01 17:40:09 +0700

    Revert "aa"
    This reverts commit bd2825078bb242fc724c8abb2e0aa023db5542ea.

--- workspace_V3ALL/ucb_batch/src/ucb/batch/util/expressions/HashtableParameterContext.java
+++ workspace_V3ALL/ucb_batch/src/ucb/batch/util/expressions/HashtableParameterContext.java
@@ -21,18 +21,43 @@ import ucb.batch.util.ParameterContext;
 public class HashtableParameterContext implements ParameterContext {
     
     @SuppressWarnings("unchecked")
-    private String context =null;
+    private Map context = new Hashtable();
+    /* (non-Javadoc)
+     * @see com.esb.fx.batch.util.ParameterContext#setValue(java.lang.String, java.lang.Object)
+     */
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.esb.common.util.ExpressionContext#setValue(java.lang.String,
+     *      java.lang.Object)
+     */
+    @SuppressWarnings("unchecked")
+    public void setValue(String name, Object value) {
+        context.put(name, value);
+    }
 
-	public void setValue(String name, Object value) {
-		// TODO Auto-generated method stub
-		
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.esb.common.util.ExpressionContext#getValue(java.lang.String)
+     */
+    public Object getValue(String name) {
+        return context.get(name);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public String toString() {
+    	StringBuffer sb = new StringBuffer();
+    	if (context != null) {
+    		Set set = context.entrySet();
+    		Iterator i = set.iterator();
+    		while (i.hasNext()) {
+    			Map.Entry entry = (Map.Entry) i.next();
+    			sb.append('[').append(entry.getKey()).append(']').append('=').append('[');
+    			sb.append(entry.getValue()).append(']').append(';');
+    		}
+    	}
+    	return sb.toString();
+    }
 
-	public Object getValue(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-   
 }
